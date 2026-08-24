592dca5d1d29222747dfee2da46450aa37dd086a
AnandRaja-tech <83357858+AnandRaja-tech@users.noreply.github.com>
2026-08-24 15:43:18 +0000

    id assigned

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'birthday-buddy',
+  appId: 'com.actionanand.birthdaybuddy.app',
+  appName: 'Birthday Buddy',
   webDir: 'www'
 };
 
